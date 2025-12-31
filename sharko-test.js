1fa53171504e8fd234c62ae86165379a1bd36928
Finn Sabia <finnwhiskey@gmail.com>
2025-12-31 12:58:24 -0500

    Delete sharko-test.js
    Signed-off-by: Finn Sabia <finnwhiskey@gmail.com>

--- sharko-test.js
+++
@@ -1,8 +0,0 @@
-// tests/sharko.test.js
-import { render } from "@testing-library/react";
-import SharkoAuth from "../src/components/SharkoAuth";
-
-test("renders login header", () => {
-  const { getByText } = render(<SharkoAuth appName="TestApp" />);
-  expect(getByText("Login to TestApp")).toBeTruthy();
-});
